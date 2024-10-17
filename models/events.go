e0d6579ef8aae859a0c39a2348f42c7320a74811
Sahal Imran <sahalimran.pro@gmail.com>
2024-10-17 18:59:01 +0000

    Added Post Route

--- models/events.go
+++ models/events.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"require"`
+	Description string    `binding:"require"`
+	Location    string    `binding:"require"`
+	DateTime    time.Time `binding:"require"`
 	UserID      int
 }
 
@@ -16,4 +16,8 @@ var events = []Event{}
 func (e Event) Save() {
 	// later: add it to database
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
