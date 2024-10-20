32735341a7b1dad5f640c375f1a38284bf14a202
Sahal Imran <sahalimran.pro@gmail.com>
2024-10-20 08:10:08 +0000

    Added event updation

--- routes/events.go
+++ routes/events.go
@@ -61,3 +61,42 @@ func getEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusOK, event)
 }
+
+func updateEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not parse event id. Try again",
+		})
+		return
+	}
+
+	_, err = models.GetEvent(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Event not found. Try again",
+		})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse request data",
+		})
+		return
+	}
+	updatedEvent.ID = id
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not update event",
+		})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Event updated successfully",
+	})
+
+}
