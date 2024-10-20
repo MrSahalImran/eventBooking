a4cfb56aa9671c3bca1a93729b49a173165e7f17
Sahal Imran <sahalimran.pro@gmail.com>
2024-10-20 18:47:58 +0000

    Added users signup route

---
+++ routes/users.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/MrSahalImran/event-booking/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not parse request data",
+		})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "could not save user. Try again later",
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "user created successfully",
+	})
+
+}
