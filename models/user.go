f1754136b63f5c4ba34845e88ca876b8dc9bb2f6
Sahal Imran <sahalimran.pro@gmail.com>
2024-10-21 17:39:15 +0000

    Added user login route

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/MrSahalImran/event-booking/db"
 	"github.com/MrSahalImran/event-booking/utils"
 )
@@ -38,4 +40,24 @@ func (u *User) Save() error {
 	u.ID = userId
 	return err
 
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+
+	err := row.Scan(&retrievedPassword)
+	if err != nil {
+		return errors.New("credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("credentials password invalid")
+	}
+
+	return nil
 }
--- routes/users.go
+++ routes/users.go
@@ -31,3 +31,26 @@ func signup(context *gin.Context) {
 	})
 
 }
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not parse request data",
+		})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"message": "could not authenticate user",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successfull"})
+
+}
